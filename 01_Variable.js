5eb1c609d91d92ee1d577115010a83e1d4b1e205
preranas28 <preranas2807@gmail.com>
2024-03-08 08:21:44 +0530

    Variables Swapping
    Variables Swapping

---
+++ 01_Variable.js
@@ -0,0 +1,23 @@
+var city; //Variable declaration
+city= "Pune"; //Variable Initialization
+console.log("City:", city);
+console.log("My city name is", city);
+
+
+//Undefined
+console.log("What is Undefined?");
+var firstName;
+console.log("First Name variable value before Initialization", firstName);
+firstName= "Prerana";
+console.log ("First name:" , firstName);
+
+firstName= "Ratan";
+console.log ("First name:" , firstName);
+
+console.log("---------------------------");
+console.log("===variable Re declaration===");
+var pinCode = "411057";
+console.log(pinCode);
+pinCode= "112233"; //variable update
+
+var pinCode= "44455" ;  // variable re declaration.
