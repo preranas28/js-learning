9360d233ce208f63bd75ea8ec305abe9a8b8f8f2
preranas28 <preranas2807@gmail.com>
2024-03-10 20:10:15 +0530

    Done with assingment day-02
    Done with assingment day-02

---
+++ 02_datatype.js
@@ -0,0 +1,26 @@
+var age=30;
+console.log("Age: " ,age);
+
+var typeOfAge = typeof age;
+console.log("Type Of Age is: " ,typeOfAge);
+
+age="Pune";
+var typeOfAge = typeof age;
+console.log("After Update age type is: " ,typeOfAge);
+
+var city; //declare
+console.log("city: " ,city);
+var typeOfCity= typeof city;
+console.log("Type Of City is: " ,typeOfCity);
+
+
+var pin = null;
+console.log("Pin:", pin);
+var typeOf = typeof pin;
+console.log("Type Of Pin is: " ,typeOf);
+
+
+var isMarried= true;
+console.log("isMarried:", isMarried);
+var typeofIsMarried = typeof isMarried;
+console.log("Type Of isMarried is: " ,typeofIsMarried);
---
+++ 02_typeOf.js
@@ -0,0 +1,39 @@
+console.log("----------Step1----------");
+var bankName; //declare 
+console.log("Value of bankName before initialization: " , bankName);
+console.log("DataType of bankName before initialization: " ,bankName);
+bankName = "SBI bank";
+typeOfBankName = typeof bankName
+console.log("Value of bankName after initialization: " , bankName);
+console.log("DataType of bankName after initialization: " ,typeOfBankName);
+
+
+console.log("----------Step2-----------");
+var percentage ="90%";
+typeOfPercentage = typeof percentage;
+console.log("DataType of percentage: " ,typeOfPercentage);
+console.log("Value of Percentage is: " ,percentage);
+
+
+console.log("----------Step3-----------");
+var isWorking;
+console.log("Value of isWorking before initialization:", isWorking);
+console.log("DataType of isWorking before initialization:", isWorking);
+isWorking = true;
+typeOfIsWorking = typeof isWorking;
+console.log("Value of isWorking after initialization:", isWorking);
+console.log("DataType of isWorking after initialization:", typeOfIsWorking);
+
+
+console.log("----------Step4-----------");
+var totalCount = "Hundred and Seven";
+typeOftotalCount = typeof totalCount;
+console.log("Value of totalCount before updation:", totalCount);
+console.log("Type of totalCount before updation:", typeOftotalCount);
+
+var totalCount = 107;
+typeOfTotalCount = typeof totalCount;
+console.log("Value of totalCount after updation:", totalCount);
+console.log("Type of totalCount after updation:", typeOfTotalCount);
+
+
