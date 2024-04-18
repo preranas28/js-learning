e5dfa64599d24dc06b810a8196fc1952a6c410d1
preranas28 <preranas2807@gmail.com>
2024-04-18 11:08:20 +0530

    DOM events and Modals

--- 28_script.js
+++ 28_script.js
@@ -33,14 +33,65 @@ for (const element of elementsUi) {
     console.log(element.innerHTML);
 }
 
+
 console.log(`----- Modifying an existing element text -----`);
 const techStackElement = document.querySelector('#techStack');
 techStackElement.innerHTML = "My Expertise in UI";
 
 
-
 console.log(`-----Changing CSS------`);
 techStackElement.style.color ="orange";
+
+
+console.log(`-----Creating an element-----`);
+const h2Element = document.createElement("h2");
+const textNodeProject = document.createTextNode("Project Name: College Management System.")
+h2Element.appendChild(textNodeProject);
+h2Element.style.color = "green";
+
+const projectNodeDiv = document.querySelector('.project');
+projectNodeDiv.appendChild(h2Element);
+
+
+console.log(`---Removing an element---`);
+const skillUlNode = document.querySelector('#skill');
+const JsLiNode = document.querySelector('#js');
+skillUlNode.removeChild(JsLiNode);
+
+
+
+function eventClicks(){
+    console.log("Hey... He clicked me....");
+}
+
+const addButton = document.querySelector('#add');
+addButton.addEventListener('click', () =>{              //arrow function.
+    console.log('Add Button Click');
+    // alert('Heyy.. you clicked me...');
+    // const num=    prompt("Enter number:", 0);
+    // console.log(num);
+    // const result =confirm("Are you sure?");
+    // console.log(result);
+
+});
+
+// addButton.addEventListener('click', function(){
+//     console.log('Add button click');
+// });
+
+
+const addButton1 = document.querySelector('#add');
+addButton1.addEventListener('click', () => {
+    const num = prompt("enter the valid number to perform square", 1);
+    // alert(num * num);
+    const elementPara = document.createElement('p');
+    const textNodeSquare= document.createTextNode(`Number ${num} square is ${num * num}`);
+    elementPara.style.color ='orange';
+    elementPara.appendChild(textNodeSquare)
+
+    const squareResultElement = document.querySelector('.squareResult');
+    squareResultElement.appendChild(elementPara);
+});
 
 
 
